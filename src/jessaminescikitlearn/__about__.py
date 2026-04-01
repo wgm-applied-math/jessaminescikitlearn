7d89d0cfa971aeaba6bc0b50c1b48372eae83aeb
W. Garrett Mitchener <garrett.mitchener@gmail.com>
2026-04-01 11:57:18 -0400

    Starting to incorporate stuff from Aidan's repo

--- src/jessaminescikitlearn/__about__.py
+++ src/jessaminescikitlearn/__about__.py
@@ -1,4 +1,4 @@
 # SPDX-FileCopyrightText: 2026-present W. Garrett Mitchener <garrett.mitchener@gmail.com>
 #
-# SPDX-License-Identifier: MIT
+# SPDX-License-Identifier: GPL-3.0-or-later
 __version__ = "0.0.1"
--- src/jessaminescikitlearn/__init__.py
+++ src/jessaminescikitlearn/__init__.py
@@ -1,3 +1,3 @@
 # SPDX-FileCopyrightText: 2026-present W. Garrett Mitchener <garrett.mitchener@gmail.com>
 #
-# SPDX-License-Identifier: MIT
+# SPDX-License-Identifier: GPL-3.0-or-later
