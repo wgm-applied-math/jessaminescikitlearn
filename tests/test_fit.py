e178c6e4aff9bdde3413f9a361568a18cb2f15fe
W. Garrett Mitchener <garrett.mitchener@gmail.com>
2026-04-22 10:11:46 -0400

    Test new pandas DataFrame capability

--- tests/test_fit.py
+++ tests/test_fit.py
@@ -3,6 +3,8 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import pytest
+
+import pandas as pd
 import numpy as np
 from scipy import stats
 import sympy
@@ -19,7 +21,7 @@ def f(x1, x2):
 def make_data():
     x1_dist = stats.Normal(mu=0.0, sigma=1.0)
     x2_dist = stats.Normal(mu=0.0, sigma=2.0)
-    np.random.seed(95548004)
+    rng = np.random.default_rng(95548004)
     n_points = 30
     # Just so this is clear,
     # for the types to check out properly in Julia
@@ -27,8 +29,8 @@ def make_data():
     # X needs to be a matrix in column-tabular form
     # y needs to be a vector, not a single-column matrix
     # which means x1 and x2 have to start as vectors
-    x1 = x1_dist.sample(shape=(n_points,))
-    x2 = x2_dist.sample(shape=(n_points,))
+    x1 = x1_dist.sample(shape=(n_points,), rng=rng)
+    x2 = x2_dist.sample(shape=(n_points,), rng=rng)
     y = f(x1, x2)
     # This is how to stack x1 and x2 as columns
     # into a matrix in column-tabular form
@@ -36,6 +38,16 @@ def make_data():
         [x1.reshape(n_points,1),
          x2.reshape(n_points,1)])
     return (X, y)
+
+
+def make_data_as_dataframe():
+    X, y = make_data()
+    y_s = pd.Series(y, name="y")
+    u = pd.Series(X[:,0], name="u")
+    v = pd.Series(X[:,1], name="v")
+    X_df = pd.DataFrame({ "u": u, "v": v })
+    return (X_df, y_s)
+
 
 def test_apply():
     raw_str = "((0.544091161224765 * x1) + ((-2.999999999999381 * (x1 * x2)) + ((0.8186362795911761 * (2.443087424614468 + (3 * x1))) + (2.9999999999994373 * x2))))"
@@ -49,13 +61,21 @@ def test_apply():
     assert discrepancy < 1e-10
 
 def test_fit_predict():
+    X, y = make_data()
+    fit_and_predict(X, y)
+
+def test_fit_predict_dataframe():
+    X, y = make_data_as_dataframe()
+    fit_and_predict(X, y)
+
+def fit_and_predict(X, y):
     # disable for a moment
     # return
-    X, y = make_data()
     r = JR.Regressor()
     r.fit(X, y)
     print(r.raw_reg_str)
     print(r.sym)
+    print(r.model())
     yHat = r.predict(X)
     discrepancy = sum((yHat - y)**2)
     print(f"test_fit_predict: discrepancy = {discrepancy}")
