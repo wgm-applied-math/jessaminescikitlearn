5d5cb0f444b756257fe578170b94325e153cd9d0
W. Garrett Mitchener <garrett.mitchener@gmail.com>
2026-05-13 17:18:58 -0400

    Set version

--- src/jessaminescikitlearn/__about__.py
+++ src/jessaminescikitlearn/__about__.py
@@ -1,4 +1,4 @@
 # SPDX-FileCopyrightText: 2026-present W. Garrett Mitchener <garrett.mitchener@gmail.com>
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
-__version__ = "0.0.1"
+__version__ = "0.3.0"
